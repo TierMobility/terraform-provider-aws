1664f8d27b69540a2b476333ce134ee67c3aa123
Nils Thenhausen <nt@ovt.me>
2020-12-03 17:31:48 +0100

    correct imports

--- aws/resource_aws_iot_thing_group.go
+++ aws/resource_aws_iot_thing_group.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/iot"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
-	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
 	"log"
 )
 
