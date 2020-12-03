3d91a88466277312ca17b5b27b3dd635fb4d4820
Nils Thenhausen <nt@ovt.me>
2020-12-03 17:51:44 +0100

    confused pikachu import

--- aws/resource_aws_iot_thing_group.go
+++ aws/resource_aws_iot_thing_group.go
@@ -6,7 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/iot"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/hashicorp/terraform-provider-aws/aws/internal/keyvaluetags"
+	"github.com/terraform-providers/terraform-provider-aws/aws/internal/keyvaluetags"
 	"log"
 )
 
